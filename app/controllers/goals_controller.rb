b9adf524b57a1e6faf691fd6ca4021074b021653
Akshay <ak_arora@hotmail.com>
2016-07-25 03:22:53 +0530

    Fixes

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,12 +4,13 @@ class GoalsController < ApplicationController
   end
 
   def create
-    goal = Goal.new(:goals => params[:goal][:goals])
+    goal = current_user.goals.new(:goals => params[:goal][:goals], date: params[:goal][:date])
     respond_to do |format|
       if goal.save
         return redirect_to root_path, notice: "Goal saved sucessfully!"
       else
-        format.js {}
+        @error = goal.errors.full_messages.first
+        format.js { @error }
       end
     end
   end
@@ -24,7 +25,7 @@ class GoalsController < ApplicationController
   end
 
   def fetch_goal
-    @goal = Goal.new
+    @goal = current_user.goals.where(date: params[:date]).first_or_create
     respond_to do |format|
       format.js
     end
