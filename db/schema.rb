c2bd4b7bf572d4e8743218a2d92753d4c3a0c83f
mfsi-akshay <akshaya@mindfiresolutions.com>
2016-07-26 21:33:59 +0530

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715150046) do
+ActiveRecord::Schema.define(version: 20160725095022) do
 
   create_table "credentials", force: :cascade do |t|
     t.integer "user_id",  limit: 4
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 20160715150046) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.integer  "user_id",    limit: 4
-    t.text     "goals",      limit: 65535
-    t.time     "date"
+    t.text     "body",       limit: 65535
+    t.datetime "date"
   end
 
   create_table "users", force: :cascade do |t|
