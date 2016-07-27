232a1c989be0a5298158058926bdf8d6ab309494
mfsi-akshay <akshaya@mindfiresolutions.com>
2016-07-27 16:08:57 +0530

    Time zone issue

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :body
-
+  self.skip_time_zone_conversion_for_attributes = [:date]
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module AddMyGoals
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.time_zone = "Kolkata"
+    config.active_record.time_zone_aware_attributes = false
   end
 end
